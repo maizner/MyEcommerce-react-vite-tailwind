e2ab0661c1259b0c52913f5eab8b04f2bb148a9d
Maia <maiaaizner@gmail.com>
2024-09-23 13:42:11 -0300

    changes path for deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import App from "./pages/App/index.jsx"
-import "./pages/App/App.css"
+import App from "./Pages/App/Index"
+import "./Pages/App/App.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
